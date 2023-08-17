ff835d97d8999f45e9d9c59a2c950d9062714d5b
jetrico <jetriconew@gmail.com>
2023-08-17 15:39:39 +0800

    Attempt 2

--- .github/spriggit/spriggit.js
+++ .github/spriggit/spriggit.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
+console.log('Processing ESP/ESM file', process.argv[2]);
+
 fs.writeFileSync(path.resolve(process.cwd(), 'IronWarAxe.yaml'), `name: test-${Math.floor(Math.random() * 1000)}`);
