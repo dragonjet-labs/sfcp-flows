a59dfa5dc1a5a1caa0c20dc99759c4d779dd2c50
jetrico <jetriconew@gmail.com>
2023-08-18 15:05:37 +0800

    Spriggit CLI

--- .github/spriggit/spriggit.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-console.log('Processing ESP/ESM file', process.argv[2]);
-
-fs.writeFileSync(path.resolve(process.cwd(), 'IronWarAxe.yaml'), `name: test-${Math.floor(Math.random() * 1000)}`);
